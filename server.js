053c12839124852756386e8da9e8f4291465e07a
Andrew Baxter <asbaxter1@gmail.com>
2022-08-09 16:29:06 -0400

    server now displays the css and js files to the webpage

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     res.render('index')
 })
 
-app.get('/notes', (req, res) => {
+app.get("/notes", (req, res) => {
     res.render('notes')
 })
-
 
 app.listen(3000)
 
