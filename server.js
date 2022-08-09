54e029adb3a9fdc66d04b997fef371009fadd5ec
Andrew Baxter <asbaxter1@gmail.com>
2022-08-09 18:07:27 -0400

    user can now POST notes and they are console logged

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express')
+
 const app = express()
 
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
 app.use(express.static("public"))
+
+
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     res.render('index')
 })
 
-app.get("/notes", (req, res) => {
-    res.render('notes')
-})
+ app.get("/notes", (req, res) => {
+     res.render('notes')
+ })
+
+  app.post("/notes", (req, res) => {    
+      console.log(req.body)
+ });
 
 app.listen(3000)
 
