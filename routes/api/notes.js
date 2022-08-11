fec61012fb4c4e63d777613cae09bc82f6c54396
Andrew Baxter <asbaxter1@gmail.com>
2022-08-10 22:55:55 -0400

    server now gets individual note by id(this should be helpful for deleting later it think)

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,10 +1,22 @@
+const { json } = require('express');
 const express = require('express');
+const { reset } = require('nodemon');
 const uuid = require('uuid');
 const router = express.Router();
 const notes = require('../../Notes');
 
 // gets notes from notepad.js and returns the JSON inside
 router.get('/', (req, res) => res.json(notes));
+
+router.get('/:id', (req, res) => {
+    const found = notes.some(note => note.id === parseInt(req.params.id));
+    
+    if (found){
+        res.json(notes.filter(note => note.id === parseInt(req.params.id)));
+    } else {
+        res.status(400),json({ msg: `No note with the id of ${req.params.id}`})
+    }
+});
 
 router.post('/', (req, res) => {
     const addedNote = {
@@ -20,5 +32,24 @@ router.post('/', (req, res) => {
     notes.push(addedNote);
     res.json(notes);
 });
+
+router.put('/:id', (req, res) => {
+    const found = notes.some(note => note.id === parseInt(req.params.is));
+
+    if(found) {
+        const updNote = req.body;
+        notes.forEach(note => {
+            if(note.id === parseInt(req.params.id)){
+                note.title = updNote.title ? updNote.title : updNote.title;
+                note.text = updNote.text ? updNote.text : updNote.text;
+
+                res.json({ msg: 'Note was updated', note});
+            }
+        });
+    }
+    else {
+        res.status(400).json({ msg: `No notes found with the id of ${req.params.id}`});
+    }
+})
 
 module.exports = router;
