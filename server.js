7e5d25e95a5ded4ee9dd38c1cf48f61c00a39097
Andrew Baxter <asbaxter1@gmail.com>
2022-08-14 10:43:03 -0400

    got the MVP working

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// gets entire public folder and its content as static
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use('/api/notes', require('./routes/api/notes'))
 
@@ -15,3 +14,11 @@ app.use('/api/notes', require('./routes/api/notes'))
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
